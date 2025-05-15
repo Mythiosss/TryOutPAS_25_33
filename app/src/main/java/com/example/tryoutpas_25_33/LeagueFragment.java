33b01cfabbcdbf220d8740d40fd0b2ee9bbcaa9b
Mythiosss <mythiosmain@gmail.com>
2025-05-15 13:01:39 +0700

    fetch API + fragment java

---
+++ app/src/main/java/com/example/tryoutpas_25_33/LeagueFragment.java
@@ -0,0 +1,18 @@
+package com.example.tryoutpas_25_33;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+public class LeagueFragment extends Fragment{
+
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            View view = inflater.inflate(R.layout.fragment_dashboard, container, false);
+            return view;
+        }
+
+}
